a3f27db66aa7060e075dea630fb61fe2ca43639d
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2023-05-02 11:06:26 +0200

    Faker para datos

---
+++ faker_data.py
@@ -0,0 +1,32 @@
+import csv
+from faker import Faker
+import random
+
+registros=10000
+fake=Faker()
+
+def create_csv_file():
+    with open('./tomates.csv', 'w') as csvfile:
+        fieldnames=['tipo_cultivo', 'ubicacion', 'tamanio', 'rdto', 'uso_fertilizantes']
+
+        writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        tipo_cultivo=['rastrera', 'semierecta', 'erecta']
+
+        for i in range (registros):
+            writer.writerow(
+                {
+                    'tipo_cultivo':fake.sentence(nb_words=1, ext_word_list=tipo_cultivo),
+                    'ubicacion':fake.street_address(),
+                    'tamanio':fake.random_int(min=100, max=100000),
+                    'rdto':fake.random_number(digits=4)/10000,
+                    'uso_fertilizantes':fake.boolean()
+
+                }
+            )
+
+
+
+if __name__ =='__main__':
+    create_csv_file()
